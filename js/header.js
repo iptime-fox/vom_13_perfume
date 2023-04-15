26277ac020441cb8c31773cdfed1734ab74130ad
iptime-fox <dudndi826@gmail.com>
2023-04-15 23:15:02 +0900

    initial commit 0415

---
+++ js/header.js
@@ -0,0 +1,25 @@
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
+const toTop = document.querySelector('#top');
+const toBottom = document.querySelector('#bottom');
+
+window.addEventListener('scroll', function () {
+  const scrY = window.scrollY;
+  if (scrY > headerHeight) {
+    header.style.background = 'black';
+    toTop.style.display = 'flex';
+  } else {
+    header.style.background = 'none';
+    toTop.style.display = 'none';
+  }
+});
+
+toTop.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+  });
+});
+
+toBottom.addEventListener('click', function () {
+  window.scroll(0, 4000);
+});
