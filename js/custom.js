ee6cc9516504e8bbaf1bb62b835f35f79a71f96f
iptime-fox <dudndi826@gmail.com>
2023-04-29 23:08:39 +0900

    score offset 0429

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,36 @@
 /* Score section - count number */
+function countNumber() {
+  $('.count-num').each(function () {
+    var $this = $(this),
+      countTo = $this.attr('data-count');
 
-$('.count-num').each(function () {
-  var $this = $(this),
-    countTo = $this.attr('data-count');
+    $({ countNum: $this.text() }).animate(
+      {
+        countNum: countTo,
+      },
+      {
+        duration: 5000,
+        easing: 'linear',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum);
+        },
+      }
+    );
+  });
+}
 
-  $({ countNum: $this.text() }).animate(
-    {
-      countNum: countTo,
-    },
-    {
-      duration: 9000,
-      easing: 'linear',
-      step: function () {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function () {
-        $this.text(this.countNum);
-      },
-    }
-  );
+const score = document.querySelector('.score-section');
+const scoreTop = score.offsetTop;
+console.log(scoreTop);
+
+window.addEventListener('scroll', function () {
+  const pageTop = window.pageYOffset;
+  console.log(pageTop);
+  // if (pageTop > scoreTop) {
+  //   countNumber();
+  //   console.log('hello');
+  // }
 });
